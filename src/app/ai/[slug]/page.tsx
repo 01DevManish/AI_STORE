33e5d5fd5003df3ecb30ec85f81c2235d44ef285
Manish Yadav <01devmanish@gmail.com>
2025-02-20 10:18:32 +0530

    Update page.tsx

--- src/app/ai/[slug]/page.tsx
+++ src/app/ai/[slug]/page.tsx
@@ -1,7 +1,5 @@
-export async function generateStaticParams() {
-  const posts = await fetch('https://.../posts').then((res) => res.json())
+
  
-  return posts.map((post) => ({
-    slug: post.slug,
-  }))
+export default function Page() {
+  return <h1>Hello, Blog Post Page!</h1>
 }
