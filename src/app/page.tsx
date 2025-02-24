27d7f1c24bfa978b03622caa3d4b0ee5907d0bc6
Manish Yadav <01devmanish@gmail.com>
2025-02-24 22:00:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import Header from '../app/ai/page';
+
 
 interface AI {
   id: number;
